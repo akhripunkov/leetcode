383ab28923ba637a360e8f3baec4b615377c7778
Alexander Khripunkov <a.khripunkov@gmail.com>
2024-04-09 17:58:45 +0400

    Added 2073. Time Needed to Buy Tickets | Updated 205. Isomorphic Strings

--- IsomorphicStrings/Solution.cs
+++ IsomorphicStrings/Solution.cs
@@ -3,10 +3,32 @@
 public class Solution {
     public bool IsIsomorphic(string s, string t)
     {
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+
         var dict = new Dictionary<char, char>();
+        var dictT = new Dictionary<char, char>();
         for (int i = 0; i < s.Length; i++)
         {
-            if (dict.s[i])
+            if (dict.ContainsKey(s[i]) && (dict[s[i]] != t[i]))
+            {
+                return false;
+            }
+
+            if (dictT.ContainsKey(t[i]))
+            {
+                if (dictT[t[i]] != s[i])
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                dict.Add(s[i], t[i]);
+                dictT.Add(t[i], s[i]);
+            }
         }
 
         return true;
---
+++ TimeNeededToBuyTickets/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace TimeNeededToBuyTickets;
+
+public class Solution {
+    public int TimeRequiredToBuy(int[] tickets, int k)
+    {
+        int seconds = 0;
+        while (tickets[k] != 0)
+        {
+            for (int i = 0; i < tickets.Length; i++)
+            {
+                if (tickets[i] != 0)
+                {
+                    tickets[i]--;
+                    seconds++;
+                }
+                
+                if (tickets[k] == 0)
+                {
+                    break;
+                }
+            }
+        }
+
+        return seconds;
+    }
+}
