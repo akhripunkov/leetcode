20893590356a4f8b64ca70bcd285cc8a8544dd6a
Alexander Khripunkov <a.khripunkov@gmail.com>
2024-04-16 12:57:19 +0400

    42. Trapping Rain Water Solved Hard

---
+++ TrappingRainWater/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace TrappingRainWater;
+
+public class Solution {
+    public int Trap(int[] height)
+    {
+        int left = 0, right = height.Length - 1, maxleft = 0, maxRight = 0, water = 0;
+
+        while (left < right)
+        {
+            if (height[left] <= height[right])
+            {
+                if (height[left] > maxleft)
+                {
+                    maxleft = height[left];
+                }
+                else
+                {
+                    water += maxleft - height[left];
+                }
+
+                left++;
+            }
+            else
+            {
+                if (height[right] > maxRight)
+                {
+                    maxRight = height[right];
+                }
+                else
+                {
+                    water += maxRight - height[right];
+                }
+
+                right--;
+            }
+        }
+
+        return water;
+        }
+    }
